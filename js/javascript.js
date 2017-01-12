843d7e8a06dc4454d18366122d35df035af0409e
jop de winter <99039923@mydavinci.nl>
2017-01-12 09:05:55 +0100

    sup

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,23 @@
-javascript
+function Test(name, id, age) {
+	this.name = name;
+	this.id = id;
+	this.age = age;
+}
+
+function Game() {
+	var randomWord;
+
+	this.start = function () {
+		randomWord = words[Math.floor(Math.random() * 479)]
+	}
+	this.test = function () {
+		console.log(randomWord)
+		console.log(document.getElementById("zaad").value)
+	}
+}
+
+var game =  new Game();
+
+(function() {
+	game.start();
+})()

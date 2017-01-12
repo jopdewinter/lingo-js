7dbccb6b9c389906826a1ccc0958eb80eb0d91ab
jop de winter <99039923@mydavinci.nl>
2017-01-12 10:15:13 +0100

    random word genereren

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,45 @@
-function Test(name, id, age) {
-	this.name = name;
-	this.id = id;
-	this.age = age;
-}
+//function Test(name, id, age) {
+//	this.name = name;
+//	this.id = id;
+//	this.age = age;
+//}
+
+//function Game() {
+//	var randomWord;
+
+//	this.start = function () {
+//		randomWord = words[Math.floor(Math.random() * 479)]
+//	}
+//	this.test = function () {
+//		console.log(randomWord)
+//		console.log(document.getElementById("zaad").value)
+//	}
+//}
+
+//var game = new Game();
+
+//(function() {
+//	game.start();
+//})()
 
 function Game() {
 	var randomWord;
 
 	this.start = function () {
-		randomWord = words[Math.floor(Math.random() * 479)]
-	}
-	this.test = function () {
-		console.log(randomWord)
-		console.log(document.getElementById("zaad").value)
+		randomWord = words[Math.floor(Math.random() * 479)];
+		console.log(randomWord);
 	}
 }
 
-var game =  new Game();
+var game = new Game();
 
 (function() {
 	game.start();
 })()
+
+function Check() {
+	var userWord = getElementById("input").value;
+	if (userWord.length != 5) {
+		alert("Het woord moet uit 5 letters bestaan")
+	}
+}

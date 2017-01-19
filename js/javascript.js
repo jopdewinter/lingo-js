72a2b28181ed06299dbdb865e6a716b5545db25d
jop de winter <99039923@mydavinci.nl>
2017-01-19 09:16:09 +0100

    check functie gemaakt

--- js/javascript.js
+++ js/javascript.js
@@ -1,27 +1,3 @@
-//function Test(name, id, age) {
-//	this.name = name;
-//	this.id = id;
-//	this.age = age;
-//}
-
-//function Game() {
-//	var randomWord;
-
-//	this.start = function () {
-//		randomWord = words[Math.floor(Math.random() * 479)]
-//	}
-//	this.test = function () {
-//		console.log(randomWord)
-//		console.log(document.getElementById("zaad").value)
-//	}
-//}
-
-//var game = new Game();
-
-//(function() {
-//	game.start();
-//})()
-
 function Game() {
 	var randomWord;
 
@@ -31,22 +7,32 @@ function Game() {
 	}
 
 	this.check = function () {
-		if (document.getElementById("input").value.length != 5) {
-			alert("Het woord moet 5 letters bevatten")
-		}
-		if (document.getElementById("input").value == randomWord) {
-			alert("Goed gedaan!")
-		}
-	}
-	this.fillin = function () {
-		for (var i = 1; i <= 5; i++) {
-			document.getElementById("letter"+i).value = document.getElementById("input").value.charAt(i-1);
-		}
-	}
-}
+			var varRWord = "appel";
+			var	varUWord = "aepes";
+			var derp = [];
 
+		    if (varRWord == varUWord) {
+      		console.log(11);
+    		} else {
+      		for (var i = 0; i < varRWord.length; i++) {
+        		if (varRWord.charAt(i) == varUWord.charAt(i)) {
+      			derp.push(1);
+    		} else {
+       			 if (varUWord.charAt(i) == varRWord.charAt(0) || varUWord.charAt(i) == varRWord.charAt(1) ||
+            		varUWord.charAt(i) == varRWord.charAt(2) || varUWord.charAt(i) == varRWord.charAt(3) ||
+            		varUWord.charAt(i) == varRWord.charAt(4)) {
+              		derp.push(2);
+            } else {
+              derp.push(0);
+            }
+   		}
+      }
+      console.log(derp);
+    }
+  }
+}
 var game = new Game();
 
 (function() {
 	game.start();
-})()
+})();
